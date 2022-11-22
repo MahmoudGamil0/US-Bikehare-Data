0a960bad411867faa1dcddff2ab673e474340771
Mahmoud Hosam Gamil <96625214+MahmoudGamil0@users.noreply.github.com>
2022-11-23 00:06:58 +0200

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -31,7 +31,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
     day = input('ENTER DAY : ').lower()
     while day not in ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'all']:
-        day = input('ENTER MONTH all, monday, tuesday, ... sunday : ').lower()
+        day = input('ENTER DAY all, monday, tuesday, ... sunday : ').lower()
     
     print('-'*40)
     return city, month, day
