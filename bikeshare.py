b05a61eeae0b26a7d32a5cd6238898b6115d76fa
Mahmoud Hosam Gamil <96625214+MahmoudGamil0@users.noreply.github.com>
2022-11-23 14:04:43 +0200

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -116,7 +116,7 @@ def station_stats(df):
 
     # TO DO: display most frequent combination of start station and end station trip
     Combination_Station = df.groupby(['Start Station', 'End Station']).count()
-    print('\nMost Commonly used combination of start station and end station trip:', Start_Station, " & ", End_Station)
+    print('\nMost Commonly used combination of start station and end station trip:', Start_Station, " -> ", End_Station)
     
     
     print("\nThis took %s seconds." % (time.time() - start_time))
